c6e0dd87fd6bc51aa5019b5ce71cd71c28e0badd
henrikedev <h.mirandacm@gmail.com>
2021-07-27 20:43:38 -0300

    Foram adicionados os eventos de adicionar e remover a quantidade

--- pizzas.js
+++ pizzas.js
@@ -1,9 +1,8 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    {id:1, name:'Acaí Puro', img:'images/acai.png', price:14.99, sizes:['100mL', '300mL', '500mL'], description:'Açaí puro sem nenhum adicional'},
+    {id:2, name:'Açaí Banana e Granola', img:'images/acai1.png', price:15.99, sizes:['100mL', '300mL', '500mL'], description:'Açaí contendo fatias de banana e granola'},
+    {id:3, name:'Açaí Banana, Kiwi e Morango ', img:'images/acai2.png', price:16.99, sizes:['100mL', '300mL', '500mL'], description:'Açaí contendo Banana, Kiwi e Morango'},
+    {id:4, name:'Açaí Banana, Morango e Granola', img:'images/acai3.png', price:17.99, sizes:['100mL', '300mL', '500mL'], description:'Açaí contendo Banana, Morango e Granola'},
+    {id:5, name:'Açaí Banana, Morango, Granola e Uva', img:'images/acai4.png', price:18.99, sizes:['100mL', '300mL', '500mL'], description:'Açaí contendo Banana, Morango, Granola e Uva'},
+    {id:6, name:'Açaí Morango, Chocolate, Paçoca e Doce', img:'images/acai5.png', price:19.99, sizes:['100mL', '300mL', '500mL'], description:'Açaí contendo Morango, Chocolate, Paçoca e Doce'}
 ];
--- script.js
+++ script.js
@@ -24,6 +24,8 @@ pizzaJson.map((item, index)=>{
 
         ////exibindo as informações da API (imagem, preço, nome e descrição das pizzas) no modal
         c('.pizzaBig img').src = item.img;
+        c('.pizzaBig img').style.width = '300px';
+        c('.pizzaBig img').style.height = '200px';
         c('.pizzaInfo h1').innerHTML = item.name;
         c('.pizzaInfo--desc').innerHTML = item.description;
         c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed('2')}`;
@@ -61,3 +63,21 @@ cs('.pizzaInfo--cancelMobileButton, .pizzaInfo--cancelButton').forEach((item)=>{
     item.addEventListener ('click', closeModal);
 })
 
+//Eventos dos botões de adicionar e remover quantidade de pizzas
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+    if(modalQt > 1){
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+    
+});
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
