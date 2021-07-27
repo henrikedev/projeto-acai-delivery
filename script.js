3ce2f7e2f3486a903c1d7534dd115294656f2851
henrikedev <h.mirandacm@gmail.com>
2021-07-27 17:23:44 -0300

    Foi removido o refresh na página ao abrir o modal e setado o delay

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ pizzaJson.map((item, index)=>{
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed('2')}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    
+    //removendo o refresh na página ao abrir o modal
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        c('.pizzaWindowArea').style.opacity = 0.5;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            c('.pizzaWindowArea').style.opacity = 1;
+        }, 200); //setando delay no modal
+        
+    });
 
     c('.pizza-area').append(pizzaItem);
-}); 
+});
