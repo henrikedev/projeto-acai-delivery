b1af2abdf88dbafcc42b193f5401bdad7b08d83f
henrikedev <h.mirandacm@gmail.com>
2021-07-27 21:53:17 -0300

    Criado os eventos de adicionar os itens ao carrinho com suas respectivas imagens, nome, tamanho e quantidades

--- script.js
+++ script.js
@@ -90,12 +90,56 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
 //Evento de adição dos produtos ao carrinho
 c('.pizzaInfo--addButton').addEventListener('click', ()=>{
     let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));//Pegando o tamanho do item
+    let identifier = pizzaJson[modalKey].id+'@'+size;
+    let key = cart.findIndex((item)=>item.identifier == identifier);
+    if (key > -1){
+        cart[key].qt += modalQt;
+    }else{
+        cart.push({
+            id:pizzaJson[modalKey].id,
+            size,
+            qt:modalQt
+         });//adicionando ao carrinho
+    }
 
-    cart.push({
-       id:pizzaJson[modalKey].id,
-       size,
-       qt:modalQt
-    });//adicionando ao carrinho
+    
 
+    updateCart();
     closeModal();   
 });
+
+function updateCart(){
+    if (cart.length > 0){
+        c('aside').classList.add('show');
+        c('.cart').innerHTML = '';
+        for (let i in cart){
+            let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            let cartItem = c('.models .cart--item').cloneNode(true);
+
+            let pizzaSizeName;
+            switch(cart[i].size){
+                case 0:
+                    pizzaSizeName = 'P';
+                    break;
+                case 1:
+                    pizzaSizeName = 'M';
+                    break;
+                case 2:
+                    pizzaSizeName = 'G';
+                    break;
+            }
+            
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+            
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+
+            c('.cart').append(cartItem);
+        }
+
+
+    }else{
+        c('aside').classList.remove('show');
+    }
+}
